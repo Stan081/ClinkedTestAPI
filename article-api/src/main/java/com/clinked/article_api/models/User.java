97172fe0525962d9dfe61d5421bee89fb3b8f615
stan081 <egbestanley84@gmail.com>
2025-05-15 02:06:45 +0100

    fix: Specify user table name to avoid clash with db reserved word "user"

--- article-api/src/main/java/com/clinked/article_api/models/User.java
+++ article-api/src/main/java/com/clinked/article_api/models/User.java
@@ -1,21 +1,27 @@
 package com.clinked.article_api.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+
 import javax.validation.constraints.*;
 
-import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -35,4 +41,35 @@ public class User {
     @Column(nullable = false)
     @NotBlank(message = "Password cannot be blank")
     private String password;
+
+    @Column(nullable = false)
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new) // Map roles to SimpleGrantedAuthority
+                .toList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
